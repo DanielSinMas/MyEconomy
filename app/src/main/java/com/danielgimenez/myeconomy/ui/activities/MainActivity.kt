2343b2996e8d2d9c8bc6ab73e461ddecbdef6f65
DanielSinMas <danielgimeneztorres@gmail.com>
2021-02-06 13:14:25 +0100

    Lateral menu: navigation drawer

--- app/src/main/java/com/danielgimenez/myeconomy/ui/activities/MainActivity.kt
+++ app/src/main/java/com/danielgimenez/myeconomy/ui/activities/MainActivity.kt
@@ -2,24 +2,48 @@ package com.danielgimenez.myeconomy.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.danielgimenez.myeconomy.R
 import com.danielgimenez.myeconomy.data.source.database.MyEconomyDatabase.Companion.getInstace
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_drawer_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mFirebaseAnalytics: FirebaseAnalytics
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_drawer_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(findViewById(R.id.toolbar))
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.FirstFragment, R.id.SecondFragment), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.drawable_icon_home)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -38,8 +62,17 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(this, LoginActivity::class.java))
                 return true
             }
+            android.R.id.home -> {
+                drawer_layout.openDrawer(Gravity.LEFT)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     fun sendEvent(id: String, name: String){
